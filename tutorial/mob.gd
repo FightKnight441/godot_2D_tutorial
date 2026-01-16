f36c345d8f41762e2d11a35e618a8df42a3b52a8
Luke <105476676+FightKnight441@users.noreply.github.com>
2026-01-15 20:00:28 -0500

    Mob stats (not yet in use)
    Not in use, currently

--- tutorial/mob.gd
+++ tutorial/mob.gd
@@ -4,7 +4,17 @@ func _ready():
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
 	$AnimatedSprite2D.play()
-	
+
+@export var maxHealth : float = 120
+@export var health : float = 120
+@export var maxStamina : float = 120
+@export var stamina : float = 120
+@export var defense : float = 12
+@export var resistence : float = 12
+@export var strength : float = 12
+@export var spirit : float = 12
+@export var speed : float = 400 # How fast the player will move (pixels/sec).
+
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	print("Debug: Mob deleted!")
