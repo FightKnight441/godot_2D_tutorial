e6d57bf5688431b8b7c1fda4d7aa7a81d1893857
Luke <105476676+FightKnight441@users.noreply.github.com>
2026-02-05 20:01:37 -0500

    Projectile Timer
    Projectile now deletes after 5 seconds instead of being tied to screen. Timer child added to Projectile
    
    Debug option on projectile so that debug messages can be printed with toggle

--- tutorial/projectile.gd
+++ tutorial/projectile.gd
@@ -1,30 +1,33 @@
 extends Area2D
 
+@export var debug : bool #toggle in inspector to get Debug messages
+
+var lifeTime : int = 5 # seconds that the projectile can live
+var speed : float = 1000.0 # speed in pixels of projectile
+var direction : Vector2 = Vector2.RIGHT  # default direction set when spawned. Player changed this when firing proj's
+
+var userSpirit : float = 0
+var userStrength : float = 0
+
 func _ready():
+	$DeathTimer.timeout.connect(_on_timeout) 
+	$DeathTimer.start(lifeTime)
+	
 	var projectile = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = projectile.pick_random()
 	$AnimatedSprite2D.play()
 	$Hitbox.activate(userStrength, userSpirit)
 
-@export var speed : float = 500.0
-var direction : Vector2 = Vector2.RIGHT  # Set when spawned
-var projectileTimeOut : float
-var userSpirit : float = 0
-var userStrength : float = 0
-
 func _process(delta: float) -> void:
 	position += direction * speed * delta
-	# Remove if off screen (optional safety)
-	if not get_viewport_rect().has_point(global_position):
-		#print("Debug: Projectile Deleted")
-		queue_free()
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
+func _on_timeout():
+	if debug:
+		print("Debug: Projectile timeout")
+		
 	queue_free()
 
-# Upon colliding mob, delete mob and projectile
 func _on_body_entered(body: Node) -> void:
 	#print("Debug: Projectile collided with", body)
 	if body.is_in_group("mobs"):
-		#body.queue_free()
-		queue_free()
+		queue_free() # delete projectile when colliding with mobs group
